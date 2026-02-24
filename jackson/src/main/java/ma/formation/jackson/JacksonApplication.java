b2439b9322a63919ddceb00c907ab05ca0aea933
OULABACH Abdelbadia <abdelbadia.oulabach@mipihsib.fr>
2026-02-24 22:05:42 +0100

    une classe main pour montrer la no-safety de objectMapper entre différents threads

--- jackson/src/main/java/ma/formation/jackson/JacksonApplication.java
+++ jackson/src/main/java/ma/formation/jackson/JacksonApplication.java
@@ -21,10 +21,10 @@ public class JacksonApplication {
     SpringApplication.run(JacksonApplication.class, args);
 
     /**
-     * l'ExecutorService
-     * est une abstraction java qui permet de gerer les threads
+     * L'ExecutorService
+     * est une abstraction java qui permet la gestion des threads
      *
-     * au lieu de gerer les threads manuellement (instanciation, demmarage, etc)
+     * au lieu de gerer les threads manuellement (instanciation, demarrage, etc)
      * on delegue ca au ExecutorService
      */
     ExecutorService executorService = Executors.newFixedThreadPool(2);
@@ -36,10 +36,10 @@ public class JacksonApplication {
      * apres, on va appeller le ExectorService (gestionnaire des threads)
      * pour executer ce Runnable task
      */
-    Runnable task = () -> {
+    Runnable task1 = () -> {
       try {
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        String json = objectMapper.writeValueAsString(new Person("Abdel", 27));
+        String json = objectMapper.writeValueAsString(new Person("TEST1", 27));
         log.info("JSON produit pour le 1er thread : {}", json);
 
       } catch (Exception e) {
@@ -50,67 +50,19 @@ public class JacksonApplication {
     Runnable task2 = () -> {
       try {
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        String json = objectMapper.writeValueAsString(new Person("Omar", 59));
-        log.info("JSON produit pour le 2ème thread : {}", json);
+        String json = objectMapper.writeValueAsString(new Person("TEST2", 59));
+        log.info("JSON produit pour le 2eme thread : {}", json);
 
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
     };
 
-    executorService.submit(task);
+    executorService.submit(task1);
     executorService.submit(task2);
 
     executorService.shutdown();
 
-    /*long start = System.currentTimeMillis();
-    
-    *//*
-                                            * INITIATION A LA BIBLIOTHEQUE JACKSON
-                                            * 1- EN OjectMapper CLASSQIUE
-                                            *//*
-                                                                                    Runtime runtime = Runtime.getRuntime();
-                                                                                    long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
-                                                                                    ObjectMapper mapper = new ObjectMapper();
-                                                                                    long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
-                                                                  
-                                                                                    System.out.println("memoire consomee apres instanciation de objectmapper" + (memoryAfter - memoryBefore));
-                                                                                    JsonNode root = mapper.readTree(new File("C:\\Users\\oulabacha\\Downloads\\hp.json"));
-                                                                  
-                                                                                    *//*
-                                                                                                                            * EN UTILISANT JsonParser BAS NIVEAU
-                                                                                                                            * LA IL FAUT CREER UN JsonFactory ET L'UTILISER POUR CREER UN JsonParer SUR UN JSON FILE
-                                                                                                                            *//*
-                                                                                                                                                                    JsonFactory factory = new JsonFactory();
-                                                                                                                                                                    List<Node> nodes = new ArrayList<Node>();
-                                                                                                                                                                    try (JsonParser parser = factory.createParser(new File("C:\\Users\\oulabacha\\Downloads\\hp.json"))) {
-                                                                                                                                                  
-                                                                                                                                                                    while (!parser.isClosed()) {
-                                                                                                                                                                     *//*
-                                                                                                                                                                                                             * CHAQUE RENTREE DANS CE BLOC REPRESENTE
-                                                                                                                                                                                                             * UN TOKEN
-                                                                                                                                                                                                             *//*
-                                                                                                                                                                                                                                                     JsonToken token = parser.nextToken();
-                                                                                                                                                                                                                                                     if (token == null)
-                                                                                                                                                                                                                                                     break;
-                                                                                                                                                                                                                                   
-                                                                                                                                                                                                                                                     if (token == JsonToken.PROPERTY_NAME && parser.currentName().equals("nodes")) {
-                                                                                                                                                                                                                                                     parser.nextToken();
-                                                                                                                                                                                                                                                     parser.nextToken();
-                                                                                                                                                                                                                                                     MappingIterator<Node> it = mapper.readerFor(Node.class).readValues(parser);
-                                                                                                                                                                                                                                   
-                                                                                                                                                                                                                                                     while (it.hasNext()) {
-                                                                                                                                                                                                                                                      nodes.add(it.next());
-                                                                                                                                                                                                                                                     }
-                                                                                                                                                                                                                                   
-                                                                                                                                                                                                                                                     }
-                                                                                                                                                                                                                                   
-                                                                                                                                                                                                                                                     }
-                                                                                                                                                                                                                                                     long end = System.currentTimeMillis();
-                                                                                                                                                                                                                                                     System.out.println(nodes.size());
-                                                                                                                                                                                                                                   
-                                                                                                                                                                                                                                                     System.out.println("Time taken: " + (end - start) + "millisecondes");
-                                                                                                                                                                                                                                                     }*/
   }
 
   static class Person {
@@ -123,6 +75,7 @@ public class JacksonApplication {
       this.name = name;
       this.age = age;
     }
+
   }
 
 }
